e11de08627fbcb59716a5a04d43744a01ce89965
Vivekjoshi11 <108278744+Vivekjoshi11@users.noreply.github.com>
2023-10-31 22:07:29 +0530

    spotify_clone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { HomeComponent } from './home/home.component';
+// import { PlaylistComponent } from './playlist/playlist.component';
+// import { SearchComponent } from './search/search.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { SongCardComponent } from './components/song-card/song-card.component';
+import { TopNavComponent } from './components/top-nav/top-nav.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    SongCardComponent,
+    TopNavComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
